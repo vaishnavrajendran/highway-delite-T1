3e6efaa7ce40cb9fc0ff13815a793af05592b8f4
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-08-27 11:49:09 +0530

    removed unused lint

--- client/src/screens/OtpVerification.tsx
+++ client/src/screens/OtpVerification.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from "react";
 import loginImage from '../assets/LoginPage.png'
 import { NavigateFunction, useNavigate } from 'react-router'
